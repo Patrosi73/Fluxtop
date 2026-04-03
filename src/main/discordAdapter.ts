8dcadccfe043befc703a82a8c00f7241c1e83b71
Patrosi73 <52974750+Patrosi73@users.noreply.github.com>
2026-04-03 19:38:57 +0200

    pls pass validation pleasee i beg

--- src/main/discordAdapter.ts
+++ src/main/discordAdapter.ts
@@ -1,14 +1,8 @@
 /*
-
  * Vesktop, a desktop app aiming to give you a snappier Discord Experience
-
  * Copyright (c) 2026 Patrosi73 and Vesktop contributors
-
-
  * SPDX-License-Identifier: GPL-3.0-or-later
-
  */
-
 
 import { ChildProcess, spawn } from "child_process";
 import { UtilityProcess, utilityProcess } from "electron";
